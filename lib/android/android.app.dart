52cbead68aea4f93abcf757950802c708c62d27b
Rodrigo Brunet <rodrigosistemas2016@gmail.com>
2020-07-11 21:41:09 -0300

    first commit

---
+++ lib/android/android.app.dart
@@ -0,0 +1,18 @@
+
+import 'package:contact/android/styles.dart';
+import 'package:contact/android/views/home.view.dart';
+import 'package:flutter/material.dart';
+
+class AndroidApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contacts',
+      debugShowCheckedModeBanner: false,
+      theme: androidTheme(),
+      home: Scaffold(
+        body: HomeView(),
+      ),
+    );
+  }
+}
---
+++ lib/android/views/contact-list-item.view.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:contact/android/views/details.view.dart';
+import 'package:contact/models/contact.model.dart';
+import 'package:flutter/material.dart';
+
+class ContactListItem extends StatelessWidget {
+  
+  ContactListItem({
+    @required this.model
+  });
+  
+  final ContactModel model;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      
+          leading: Container(
+            width: 48,
+            height: 48,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(48),
+              image: DecorationImage(
+                image: model.image == null ? 
+                AssetImage('assets/images/profile-picture.png'): FileImage(
+                  File(model.image),
+                ),
+                ),
+            ),
+          ),
+          title: Text(model.name),
+          subtitle: Text(model.phone),
+          trailing: FlatButton(
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => DetailsView(),
+                ),
+              );
+            }, 
+            child: Icon(
+              Icons.person,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+        
+    );
+  }
+}
